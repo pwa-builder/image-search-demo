76e75d3d81974e8ada76d0ade731219d80d64e6d
Justin Willis <jgw9617@gmail.com>
2024-09-05 19:02:44 +0000

    use webnn

--- src/services/image-ai.ts
+++ src/services/image-ai.ts
@@ -1,18 +1,27 @@
-import { ImageToTextPipeline, pipeline } from "@huggingface/transformers";
+import { ImageToTextPipeline, ImageClassificationPipeline, pipeline } from "@huggingface/transformers";
 
 let captioner: ImageToTextPipeline | undefined = undefined;
-
+let ocrPipeline: ImageToTextPipeline | undefined = undefined;
+let classifyPipeline: ImageClassificationPipeline | undefined = undefined;
 
 self.onmessage = async (e) => {
     if (e.data.type === 'caption') {
-        return new Promise((resolve) => {
-            captionImage(e.data.blob).then((caption: any) => {
-                self.postMessage({
-                    type: 'caption',
-                    caption: caption
-                });
-                resolve(caption);
-            })
+        return new Promise(async (resolve) => {
+            const caption = await captionImage(e.data.blob);
+            const classification = await classifyImage(e.data.blob);
+            const foundText = await getTextFromImage(e.data.blob);
+
+            const imageData = {
+                caption,
+                text: foundText,
+                classification
+            };
+
+            self.postMessage({
+                type: 'processed',
+                data: imageData
+            });
+            resolve(imageData);
         })
     }
     else {
@@ -20,10 +29,42 @@ self.onmessage = async (e) => {
     }
 }
 
+async function classifyImage(blob: Blob) {
+   if (!classifyPipeline) {
+      classifyPipeline = await pipeline('image-classification', 'onnx-community/mobilenet_v2_1.4_224', {
+        device: "webnn"
+      })
+   }
+
+   const url = URL.createObjectURL(blob);
+
+   const result = await classifyPipeline(url);
+   console.log('classifyResult', result);
+   URL.revokeObjectURL(url);
+
+   return result;
+}
+
+async function getTextFromImage(blob: Blob) {
+    if (!ocrPipeline) {
+        ocrPipeline = await pipeline('image-to-text', 'Xenova/trocr-small-printed', {
+            device: "webnn"
+        });
+    }
+
+    const url = URL.createObjectURL(blob);
+
+    const result = await ocrPipeline(url);
+    console.log("ocr result", result);
+    URL.revokeObjectURL(url);
+
+    return result;
+}
+
 async function captionImage(blob: Blob) {
     if (!captioner) {
         captioner = await pipeline('image-to-text', 'xenova/vit-gpt2-image-captioning', {
-            device: "webgpu"
+            device: "webnn"
         });
     }
 
